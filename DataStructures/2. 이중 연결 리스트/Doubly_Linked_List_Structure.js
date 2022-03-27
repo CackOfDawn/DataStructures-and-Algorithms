6c77a58741535c70b03ae127ab500dbb16de8dd9
NaLeum <wognsdl@likelion.org>
2022-03-28 07:45:28 +0900

    양방향 리스트 push pop shift unshift

---
+++ DataStructures/2. 이중 연결 리스트/Doubly_Linked_List_Structure.js
@@ -0,0 +1,87 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  pop() {
+    if (!this.head) return undefined;
+    const popedNode = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = popedNode.prev;
+      this.tail.next = null;
+      popedNode.prev = null;
+    }
+    this.length--;
+    return popedNode;
+  }
+  shift() {
+    if (!this.head) return undefined;
+    const oldHead = this.head;
+    if (this.length === 1) {
+      this.tail = null;
+      this.head = null;
+    } else {
+      this.head = oldHead.next;
+      this.head.prev = null;
+      oldHead.next = null;
+    }
+    this.length--;
+
+    return oldHead;
+  }
+  unshift(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.head.prev = newNode;
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  print() {
+    const arr = [];
+    let current = this.head;
+    while (current) {
+      arr.push(current.val);
+      current = current.next;
+    }
+    console.log(arr);
+  }
+}
+const list = new DoublyLinkedList();
+list.push('HELLO');
+list.push('GOODBYE');
+list.push('!');
+console.log(list.unshift(1));
+console.log(list);
+list.print();
