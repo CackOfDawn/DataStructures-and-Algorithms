7d87b5d6560b5a80f2f07ed3bca33f8cfba24ba6
NaLeum <wognsdl@likelion.org>
2022-03-29 07:42:31 +0900

    이중 연결 리스트 마무리

--- DataStructures/2. 이중 연결 리스트/Doubly_Linked_List_Structure.js
+++ DataStructures/2. 이중 연결 리스트/Doubly_Linked_List_Structure.js
@@ -67,6 +67,70 @@ class DoublyLinkedList {
     this.length++;
     return this;
   }
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+    if (index <= this.length / 2) {
+      let counter = 0;
+      let current = this.head;
+      while (counter !== index) {
+        current = current.next;
+        counter++;
+      }
+      return current;
+    } else {
+      let counter = this.length - 1;
+      let current = this.tail;
+      while (counter !== index) {
+        current = current.prev;
+        counter--;
+      }
+      return current;
+    }
+  }
+
+  set(index, val) {
+    const foundNode = this.get(index);
+    if (!foundNode) return false;
+    foundNode.val = val;
+    return true;
+  }
+
+  insert(index, val) {
+    if (index < 0 || index > this.length) return false;
+    if (index === 0) return !!this.unshift(val);
+    if (index === this.length) return !!this.push(val);
+
+    const insertNode = new Node(val);
+    const beforeNode = this.get(index - 1);
+    const afterNode = beforeNode.next;
+
+    beforeNode.next = insertNode;
+    insertNode.prev = beforeNode;
+    insertNode.next = afterNode;
+    afterNode.prev = insertNode;
+
+    this.length++;
+    return true;
+  }
+
+  remove(index) {
+    if (index < 0 || index >= this.length) return undefined;
+    if (index === 0) return this.shift();
+    if (index === this.length - 1) return this.pop();
+
+    const removeNode = this.get(index - 1);
+    const beforeNode = removeNode.prev;
+    const afterNode = removeNode.next;
+
+    beforeNode.next = afterNode;
+    afterNode.prev = beforeNode;
+
+    removeNode.next = null;
+    removeNode.prev = null;
+
+    this.length--;
+    return removeNode;
+  }
 
   print() {
     const arr = [];
@@ -83,5 +147,5 @@ list.push('HELLO');
 list.push('GOODBYE');
 list.push('!');
 console.log(list.unshift(1));
-console.log(list);
+console.log(list.insert(1, 'aa'));
 list.print();
