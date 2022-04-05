c801a90c62faf5d5972f979409e1b31388c38fe8
NaLeum <wognsdl@likelion.org>
2022-04-06 07:05:07 +0900

    이진 검색트리 find

--- DataStructures/4. 이진 검색 트리/Binary_Search_Tree.js
+++ DataStructures/4. 이진 검색 트리/Binary_Search_Tree.js
@@ -37,6 +37,36 @@ class BinarySearchTree {
       }
     }
   }
+  find(value) {
+    if (this.root === null) return false;
+    let current = this.root;
+    let found = false;
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        found = true;
+      }
+    }
+    if (!found) return undefined;
+    return current;
+  }
+  contains(value) {
+    if (this.root === null) return false;
+    let current = this.root;
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
 }
 
 const tree = new BinarySearchTree();
