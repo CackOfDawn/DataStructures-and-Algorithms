bed3e8e4c4326f28abf94b86862afcf289798b34
NaLeum <wognsdl@likelion.org>
2022-04-18 10:39:35 +0900

    그래프

---
+++ DataStructures/8. 그래프/Graph.js
@@ -0,0 +1,35 @@
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
+  addEdge(v1, v2) {
+    this.adjacencyList[v1].push(v2);
+    this.adjacencyList[v2].push(v1);
+  }
+  removeEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+      (v) => v !== vertex2
+    );
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex1].filter(
+      (v) => v !== vertex1
+    );
+  }
+  removeVertex(vertex) {
+    while (this.adjacencyList[vertex].length) {
+      const adjacencyVertex = this.adjacencyList[vertex].pop();
+      this.removeEdge(vertex, adjacencyVertex);
+    }
+    delete this.adjacencyList[vertex];
+  }
+}
+
+const g = new Graph();
+g.addVertex("Dallas");
+g.addVertex("Tokyo");
+g.addVertex("Aspen");
+g.addEdge("Dallas", "Tokyo");
+
+console.log(g);
