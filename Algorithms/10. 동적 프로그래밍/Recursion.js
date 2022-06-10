b74dc78d6d40f007bc8f796ef7837b92298e9927
NaLeum <wognsdl@likelion.org>
2022-06-10 17:07:49 +0900

    feat: 애너그램

--- Algorithms/10. 동적 프로그래밍/Recursion.js
+++ Algorithms/10. 동적 프로그래밍/Recursion.js
@@ -13,3 +13,12 @@ function fibmemo(n, memo = [undefined, 1, 1]) {
 }
 
 console.log(fibmemo(10, []));
+
+function fibTabulated(n) {
+  if (n <= 2) return 1;
+  const fibNums = [0, 1, 1];
+  for (let i = 3; i <= n; i++) {
+    fibNums[i] = fibNums[i - 1] + fibNums[i - 2];
+  }
+  return fibNums[n];
+}
